e81074d83149c373effbddc2bcfc5b94d8640b61
herloncosta <herlon36@gmail.com>
2023-01-07 18:18:39 -0300

    open links in other page

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -11,22 +11,22 @@ export function Contact() {
       </div>
       <div className="contact-group">
         <div>
-          <a href="https://wa.me/5571983012996">
+          <a href="https://wa.me/5571983012996" target="_blank">
             <ImWhatsapp size={40} fill="#32BD33" />
           </a>
         </div>
         <div>
-          <a href="https://www.instagram.com/herloncosta_/">
+          <a href="https://www.instagram.com/herloncosta_/" target="_blank">
             <SiInstagram size={40} fill="#E4054A" />
           </a>
         </div>
         <div>
-          <a href="https://github.com/herloncosta">
+          <a href="https://github.com/herloncosta" target="_blank">
             <ImGithub size={40} fill="#aaa" />
           </a>
         </div>
         <div>
-          <a href="https://www.linkedin.com/in/herloncosta/">
+          <a href="https://www.linkedin.com/in/herloncosta/" target="_blank">
             <SiLinkedin size={40} fill="#0A66C2" />
           </a>
         </div>
